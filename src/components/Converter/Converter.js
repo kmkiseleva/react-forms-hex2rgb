10a9b5b37881304068492cab3c703632a769d6b8
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2021-09-26 09:07:36 +0300

    decomposition

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,22 +1,6 @@
 import styles from "./Converter.module.css";
 import { useState } from "react";
-
-// функция конвертирования
-function convertColor(color) {
-  /* Check for # infront of the value, if it's there, strip it */
-  if (color.substring(0, 1) === "#") {
-    color = color.substring(1);
-  }
-
-  let rgbColor = {};
-  /* Grab each pair (channel) of hex values and parse them to ints using hexadecimal decoding */
-  rgbColor.r = parseInt(color.substring(0, 2), 16);
-  rgbColor.g = parseInt(color.substring(2, 4), 16);
-  rgbColor.b = parseInt(color.substring(4), 16);
-
-  let result = `rgb(${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b})`;
-  return result;
-}
+import convertColor from "../../utils/convertColor";
 
 const Converter = () => {
   const [hexInput, setHexInput] = useState("");
